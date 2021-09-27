ca682a81780be4dca236453f9553e1442b1e5888
Roman <Bilo_um18@student.itstep.org>
2021-09-27 21:42:36 +0300

    Add project files.

---
+++ ClassCar/Car.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace ClassCar
+{
+    public class Car
+    {
+        private int count;
+        private string brand;
+        private string model;
+        private int manufactYear;
+        private double engVolume;
+        private string color;
+        private static int id;
+        public readonly static string category;
+
+        static Car()
+        {
+            id = 0;
+            category = "Cars";
+        }
+
+        public Car()
+        {
+            id++;
+            count = id;
+            Console.Write("Input brand of car: ");
+            Brand = Console.ReadLine();
+            Console.Write("Input car's model: ");
+            Model = Console.ReadLine();
+            Console.Write("Car's manufacturing year: ");
+            ManufactYear = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Engine volume of the car: ");
+            EngVolume = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input color of the car: ");
+            Color = Console.ReadLine();
+        }
+
+        public Car(string brand, string model, int manufactYear, double engVolume, string color)
+        {
+            id++;
+            count = id;
+            Brand = brand;
+            Model = model;
+            ManufactYear = manufactYear;
+            EngVolume = engVolume;
+            Color = color;
+        }
+
+        private string Check(ref string value, string field)
+        {
+            if (!String.IsNullOrWhiteSpace(value))
+                return value;
+            else return $"No {field}";
+        }
+
+        public int Count
+        {
+            get => count;
+            set { count = value; }
+        }
+        public string Brand
+        {
+            get => brand;
+            set { brand = Check(ref value, "brand"); }
+        }
+        public string Model
+        {
+            get => model;
+            set { model = Check(ref value, "model"); }
+        }
+        public int ManufactYear
+        {
+            get => manufactYear;
+            set
+            {
+                if (value > 1900 & value < 2022)
+                    manufactYear = value;
+                else
+                    manufactYear = 1900;
+            }
+        }
+        public double EngVolume
+        {
+            get => engVolume;
+            set
+            {
+                if (value > 1.0)
+                    engVolume = value;
+                else
+                    engVolume = 1.0;
+            }
+        }
+        public string Color
+        {
+            get => color;
+            set { color = Check(ref value, "color"); }
+        }
+
+    public void Print()
+        {
+            Console.WriteLine($"ID: {Count}\tBrand: {Brand}\t\tModel: {Model}");
+            Console.WriteLine($"Manufacturing year: {ManufactYear}\tEngine volume: {EngVolume}");
+            Console.WriteLine($"Color: {Color}\n");
+        }
+    }
+}
---
+++ ClassCar/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ClassCar
+{
+    class Program
+    {
+        public static bool Answer()
+        {
+            Console.WriteLine("\nDo you want to add a car to the list? y/n");
+            if (Console.ReadKey().KeyChar == 'y')
+                return true;
+            else
+                return false;
+        }
+
+        static void Add(ref Car[] car)
+        {
+            Car[] newcars = new Car[car.Length + 1];
+            for (int i = 0; i < car.Length; i++)
+                newcars[i] = car[i];
+            newcars[newcars.Length - 1] = new Car();
+            car = newcars;
+        }
+
+        static void Main(string[] args)
+        {
+            bool changes = false;
+            Car[] car = new Car[] {
+                new Car("Mercedes", "AGL", 2020, 4.5, "Black"),
+                new Car("Audi", "A8", 2018, 3.0, "Red"),
+                new Car("Chrysler", "M300", 2016, 4.0, "Dark green"),
+                new Car("BMW", "X7", 2019, 3.8, "White"),
+                new Car("Bugatti", "Chiron", 2017, 8.0, "Metallik blue")};
+            
+            for (int i = 0; i < car.Length; i++)
+                car[i].Print();
+
+            while (Answer())
+            {
+                Console.WriteLine("\n");
+                Add(ref car);
+                changes = true;
+            }
+
+            if (!changes)
+            {
+                Console.WriteLine("\n");
+                for (int i = 0; i < car.Length; i++)
+                    car[i].Print();
+            }
+        }
+    }
+}
